66e53127e789965ca5b1bea3e575a21b494a8e98
Zhz2025 <Zhanghz270140@163.com>
2026-02-03 10:26:17 +0800

    FindAprilTag debug

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -346,7 +346,7 @@ public class Constants {
     
     //排除掉1,3,4,6,17，19，20，22号八个看不到的Tag
     public static final List<Integer> usableAprilTagIDs = Arrays.asList(
-            2,5,7,8,9,10,11,12,13,14,15,16,18, 21, 23, 24, 25, 26, 27,29, 30, 31, 32
+            2,5,7,8,9,10,11,12,13,14,15,16,18, 21, 23, 24, 25, 26, 27,28, 29, 30, 31, 32
     );
 
     public final class FieldInfo {
