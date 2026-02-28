9f9f10b0e02eda4aaa2be0d5858f4608c2879218
Chipmunk <peterrabbit2009@163.com>
2026-02-28 20:14:30 +0800

    modify const

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -446,7 +446,7 @@ public class Constants {
     public static final class FeedMotor {
         public static final int motorID = 6;
         public static final String canBusName = "rio";
-        public static final double KP = 0.4;
+        public static final double KP = 0.6;
         public static final double KI = 0;
         public static final double KD = 0;
         public static final double KS = 0;
@@ -458,7 +458,7 @@ public class Constants {
     public static final class TurrentMotor {
         public static final int motorID = 0;
         public static final String canBusName = "rio";
-        public static final double KP = 0.3;
+        public static final double KP = 0.4;
         public static final double KI = 0;
         public static final double KD = 0;
         public static final double KS = 0;
